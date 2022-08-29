ec1fc4ff1de1254f313a64b4979e46b688a3a0e2
dim-sen <dimas.10119285@mahasiswa.unikom.ac.id>
2022-08-29 23:10:29 +0700

    GridView and Styling

---
+++ lib/models/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+
+const DUMMY_CATEGORIES = [
+  Category(
+    id: 'c01',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c02',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c03',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c04',
+    title: 'German',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c05',
+    title: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c06',
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c07',
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c08',
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c09',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+];
